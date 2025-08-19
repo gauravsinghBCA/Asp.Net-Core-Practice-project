26d387574126287ee19edb7387e3a36a949fdd1a
gauravsinghBCA <gauravkrsingh27@gmail.com>
2025-08-19 22:51:18 +0530

    Practive of Strongly html helpper

---
+++ Strongly_HtmoHelpper/Strongly_HtmoHelpper/Controllers/MyNewController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Strongly_HtmoHelpper.Models;
+
+namespace Strongly_HtmoHelpper.Controllers
+{
+    public class MyNewController : Controller
+    {
+        public IActionResult Index()
+        {
+            Student s = new Student() { sname = "Gaurav", pass = "12" };
+            return View(s);
+        }
+
+        public string ShowData(Student s)
+        {
+            return s.sname + "  " + s.pass; 
+        }
+    }
+}
---
+++ Strongly_HtmoHelpper/Strongly_HtmoHelpper/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace Strongly_HtmoHelpper.Models
+{
+    public class Student
+    {
+        [DisplayName("User Name")]
+        public string sname { get; set; }
+        [DisplayName("User Password")]
+        public string pass { get; set; }
+    }
+}
