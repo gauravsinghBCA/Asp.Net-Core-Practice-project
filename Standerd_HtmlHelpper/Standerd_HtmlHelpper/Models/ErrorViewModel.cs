01583dcf75cb0dc5e3a5bd7b62173647fbeca8c1
gauravsinghBCA <gauravkrsingh27@gmail.com>
2025-08-25 23:09:37 +0530

    Practice Standerd Html Helpper

---
+++ Standerd_HtmlHelpper/Standerd_HtmlHelpper/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Standerd_HtmlHelpper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
