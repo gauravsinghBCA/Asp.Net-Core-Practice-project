2bbebfe3c294e6bdbf400a97b8a2ac62679051f6
gauravsinghBCA <gauravkrsingh27@gmail.com>
2025-09-04 22:48:07 +0530

    Custom Layout With Carousel Practice

---
+++ CustomLayoutWithCarousel/CustomLayoutWithCarousel/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomLayoutWithCarousel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About_Us()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
---
+++ CustomLayoutWithCarousel/CustomLayoutWithCarousel/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseStaticFiles();
+app.MapControllerRoute(
+     
+    name:"Default",
+    pattern:"{controller=Home}/{action=Index}"
+    );
+
+
+
+//app.MapGet("/", () => "Hello World!");
+
+app.Run();
