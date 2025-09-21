1c7f56896ae9a0ae88cf2244eeee9ce0a95ab4f1
gauravsinghBCA <gauravkrsingh27@gmail.com>
2025-09-21 17:30:07 +0530

    Custome layout practice project

---
+++ Custome_layout_practice/Custome_layout_practice/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Custome_layout_practice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Courses()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
